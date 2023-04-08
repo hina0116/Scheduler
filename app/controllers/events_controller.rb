5c920a6c81f33028e392cb9094c23cc90ff3d4af
EC2 Default User <ec2-user@ip-172-31-43-230.ap-northeast-1.compute.internal>
2023-04-08 08:43:38 +0000

    [ADD]

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,29 @@
 class EventsController < ApplicationController
-  def index
+
+  def new
+    @event = Events.new
+    render plain: render_to_string(partial: 'form_new', layout: false, locals: { event: @event })
   end
+
+  def index
+    @events = Event.all
+  end
+
+  def create
+    @event = Events.new(params_event)
+      if @event.save
+        respond_to do |format|
+          format.html { redirect_to root_path }
+      end
+      else
+        respond_to do |format|
+          format.js {render partial: "error" }
+      end
+    end
+  end
+
+  def params_event
+    params.require(:event).permit(:title, :content, :start, :finish)
+  end
+
 end
--- app/javascript/packs/calendar/event.js
+++ app/javascript/packs/calendar/event.js
@@ -7,37 +7,69 @@ document.addEventListener('turbolinks:load', function() {
   var calendarEl = document.getElementById('calendar');
 
   var calendar = new Calendar(calendarEl, {
-    plugins: [ dayGridPlugin, interactionPlugin, timegridPlugin ],
-
-    locale: 'ja',
-    timeZone: 'Asia/Tokyo',
-    firstDay: 1,
-    headerToolbar: {
-      start: 'title',
-      center: '',
-      end: 'today prev,next'
-    },
-    expandRows: true,
-    stickyHeaderDates: true,
-    buttonText: {
-       today: '今日'
-    },
-    allDayText: '',
-    height: "auto",
-    initialView: 'timeGridDay',
-    nowIndicator: true,
-
-    dateClick: function(info){
-    //日付をクリックしたときのイベント(詳しくは次回の記事へ)
-    },
-    eventClick: function(info){
-    //表示されたイベントをクリックしたときのイベント(詳しくは次回の記事へ)
-    },
-    eventClassNames: function(arg){
-    //表示されたイベントにclassをcss用に追加する(詳しくは次回の記事へ)
-    }
-
-  });
-
-  calendar.render();
+      plugins: [ dayGridPlugin, interactionPlugin, timegridPlugin ],
+  
+      locale: 'ja',
+      timeZone: 'Asia/Tokyo',
+      firstDay: 1,
+      headerToolbar: {
+        start: 'title',
+        center: '',
+        end: 'today prev,next'
+      },
+      expandRows: true,
+      stickyHeaderDates: true,
+      buttonText: {
+         today: '今日'
+      },
+      allDayText: '',
+      height: "auto",
+      initialView: 'timeGridDay',
+      nowIndicator: true,
+  
+      dateClick: function(info){
+        const year  = info.date.getFullYear();
+        const month = (info.date.getMonth() + 1);
+        const day   = info.date.getDate();
+  
+        $.ajax({
+        type: 'GET',
+        url:  '/events/new',
+        }).done(function (res) {
+  
+        $('.modal-body').html(res);
+  
+        $('#event_start_1i').val(year);
+        $('#event_start_2i').val(month);
+        $('#event_start_3i').val(day);
+  
+        $('#event_end_1i').val(year);
+        $('#event_end_2i').val(month);
+        $('#event_end_3i').val(day);
+  
+        $('#modal').fadeIn();
+  
+        }).fail(function (result) {
+        alert("failed");
+        });
+  
+      },
+      
+      eventClick: function(info){
+      //表示されたイベントをクリックしたときのイベント(詳しくは次回の記事へ)
+      },
+      eventClassNames: function(arg){
+      //表示されたイベントにclassをcss用に追加する(詳しくは次回の記事へ)
+      }
+  
+      events: '/events.json',
+  
+    });
+  
+    calendar.render();
+    
+    $(".error").click(function(){
+        calendar.refetchEvents();
+    });
+    
 });
